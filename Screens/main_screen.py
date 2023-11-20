f04ce17bcd481160602f34668c38c62503bb9221
Georgi-Sr <georgisti6@gmail.com>
2023-11-20 19:46:22 +0200

    add macros and main execution

--- Screens/main_screen.py
+++ Screens/main_screen.py
@@ -3,13 +3,17 @@ QRadioButton, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit
 from PyQt5.QtGui import QFont
 import sys
 
+SCREEN_WIDTH = 400
+SCREEN_LENGTH = 500
+FONT_SIZE = 20
+
 class Window1(QMainWindow):
     size = 0
 
     def __init__(self):
         super(Window1, self).__init__()
         self.setWindowTitle("Viktoriin")
-        self.setGeometry(0, 0, 400, 500)
+        self.setGeometry(0, 0, SCREEN_WIDTH, SCREEN_LENGTH)
         self.init_layout()
   
     def init_layout(self):
@@ -19,7 +23,7 @@ class Window1(QMainWindow):
         self.layout_1 = QHBoxLayout()
 
         self.button = QPushButton("Create Test")
-        self.button.setFont(QFont('' , 20))
+        self.button.setFont(QFont('' , FONT_SIZE))
         self.button.setStyleSheet("border :5px solid ;" "border-color : #00FFF6;; color: #00FFF6;;")
         self.main_layout.addWidget(self.button)
         self.main_layout.addLayout(self.layout_1)
@@ -29,10 +33,15 @@ class Window1(QMainWindow):
         self.setCentralWidget(widget)
         self.setCentralWidget(widget)
     
+
+
+
+
+if __name__ == "__main__":  
+    app = QApplication(sys.argv)
+    screen = Window1()
+    screen.show()
+    sys.exit(app.exec_())
     
-app = QApplication(sys.argv)
-screen = Window1()
-screen.show()
-sys.exit(app.exec_())
 
         
